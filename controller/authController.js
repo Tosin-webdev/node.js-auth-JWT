2beb39c818900035bf14f01b944158c4305ba37f
Tosin <oladejit3@gmail.com>
2021-11-23 12:15:07 +0100

    added changez

--- controller/authController.js
+++ controller/authController.js
@@ -10,7 +10,15 @@ module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
 
-module.exports.signup_post = (req, res) => {
-  res.render("signup");
-  console.log(req.body);
+module.exports.signup_post = async (req, res) => {
+  const { name, email, password, password2 } = req.body;
+
+  try {
+    const user = await User.create({ name, email, password, password2 });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("error, user not created");
+  }
+  // res.render("signup");
+  // console.log(req.body);
 };
