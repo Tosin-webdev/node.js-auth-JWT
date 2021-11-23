d1c12ee4506b9fb1a44fb87c8ed5578a28c34cc5
Tosin <oladejit3@gmail.com>
2021-11-23 12:47:47 +0100

    added changes

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,4 @@
+const User = require("../models/User");
 module.exports.login_get = (req, res) => {
   res.render("login");
 };
@@ -15,6 +16,7 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ name, email, password, password2 });
+    res.status(201).json(user);
   } catch (error) {
     console.log(error);
     res.status(400).send("error, user not created");
