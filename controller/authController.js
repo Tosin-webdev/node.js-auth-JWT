4165dccd4c0c35288a18d363db3644c8f0b4e8fa
Tosin <oladejit3@gmail.com>
2021-11-24 03:34:07 +0100

    added changes

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -21,6 +22,14 @@ const handleErrors = (err) => {
   return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, "Tosin secret", {
+    expiresIn: maxAge,
+  });
+};
+
 module.exports.login_get = (req, res) => {
   res.render("login");
 };
@@ -38,11 +47,14 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ name, email, password, password2 });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    console.log(user._id);
+    res.status(201).json({ user: user._id });
   } catch (error) {
     // console.log(error);
     const errors = handleErrors(error);
-    res.status(400).send(errors);
+    res.status(400).json(errors);
   }
   // res.render("signup");
   // console.log(req.body);
