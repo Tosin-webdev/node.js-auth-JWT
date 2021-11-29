43b74174fc35c4767ea08aff5c479309ae884d7c
Tosin <oladejit3@gmail.com>
2021-11-29 20:46:50 +0100

    added changes

--- controller/authController.js
+++ controller/authController.js
@@ -43,12 +43,7 @@ module.exports.login_get = (req, res) => {
 };
 
 module.exports.signup_post = async (req, res) => {
-  const { name, email, password, password2 } = req.body;
-
-  // if (password != password2) {
-  //   console.log("wrong credentials");
-  // }
-
+  const { name, email, password } = req.body;
   try {
     const user = await User.create({ name, email, password });
     const token = createToken(user._id);
--- models/User.js
+++ models/User.js
@@ -26,9 +26,22 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
-  // console.log("new user was created and saved", doc);
 });
+// console.log("new user was created and saved", doc);
+// static method to login user
 
-const User = mongoose.model("auth", userSchema);
+// userSchema.statics.login = async function(email, password ){
+//   const user = await this.findOne({email})
 
-module.exports = User;
+//    if (user) {
+//     const auth = await bcrypt.compare(password, user.password);
+//     if (auth) {
+//       return user;
+//     }
+//     throw Error("incorrect password");
+//   }
+//   throw Error("incorrect email");
+// }
+
+// const User = mongoose.model(user, userSchema,)
+// module.exports = User;
