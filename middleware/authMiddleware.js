4adf3b367dbbad1d8d14acc644530ee8fc5a04d0
Tosin <oladejit3@gmail.com>
2021-11-30 14:59:14 +0100

    update file

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,4 +19,25 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, "tosin ninja secret", async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        // res.redirect("/login");
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+  }
+};
+
+module.exports = { requireAuth, checkUser };
