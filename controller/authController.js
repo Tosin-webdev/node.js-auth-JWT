a7a43b5d7db5c65181645fa10336c0c913e824c0
Tosin <oladejit3@gmail.com>
2021-12-01 06:37:25 +0100

    update file

--- controller/authController.js
+++ controller/authController.js
@@ -83,5 +83,5 @@ module.exports.login_post = async (req, res) => {
 module.exports.logout_get = (req, res) => {
   res.cookie("jwt", "", { maxAge: 1 });
   res.redirect("/");
-  res.send("hello");
+  // res.send("hello");
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,6 @@ route.get("/login", authController.login_get);
 route.post("/login", authController.login_post);
 route.get("/signup", authController.signup_get);
 route.post("/signup", authController.signup_post);
-route.post("/logout", authController.logout_get);
+route.get("/logout", authController.logout_get);
 
 module.exports = route;
